c4bc1683fb2157607fcad68f9123dceeaca3decb
CLINET66 <alexander66andre@gmail.com>
2022-09-10 14:19:54 -0700

    commit 5,6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -16,10 +16,8 @@ int main(void)
 
 {
 
-	printf("with proper grammar, but the outcome is a piece of
+printf("with proper grammar, but the outcome is a piece of art,\n");
 
-			art,\n");
-
-	return (0);
+return (0);
 
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,26 +14,26 @@ int main(void)
 
 {
 
-	char a;
+char a;
 
-	int b;
+int b;
 
-	long int c;
+long int c;
 
-	long long int d;
+long long int d;
 
-	float e;
+float e;
 
-	printf("Size of a char: %d byte(s)\n", sizeof(a));
+printf("Size of a char: %d byte(s)\n", sizeof(a));
 
-	printf("Size of an int: %d byte(s)\n", sizeof(b));
+printf("Size of an int: %d byte(s)\n", sizeof(b));
 
-	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+printf("Size of a long int: %d byte(s)\n", sizeof(c));
 
-	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+printf("Size of a long long int: %d byte(s)\n", sizeof(d));
 
-	printf("Size of a float: %d byte(s)\n", sizeof(e));
+printf("Size of a float: %d byte(s)\n", sizeof(e));
 
-	return (0);
+return (0);
 
 }
