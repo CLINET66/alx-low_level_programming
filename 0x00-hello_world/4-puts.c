742b9758b1383b66b578ad554715c33d72a822b8
CLINET66 <alexander66andre@gmail.com>
2022-09-10 14:02:25 -0700

    commit 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
 
-
-
 /**
- * main - Printing
- *
- * Return: 0
- *
- */
-
+* main - print the string in the put function
+*
+* Description: using the main function
+* this programme prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
 int main(void)
-
 {
-
-	puts("\"Programming is like building a multilingual
-
-			puzzle");
-
-	return (0);
+       puts("\"Promgramming is like building a multilingual puzzle");
+       return (0);
 }
+
