208d7786505e49dd3a75428522518c9c1ec34011
CLINET66 <alexander66andre@gmail.com>
2022-09-10 14:07:32 -0700

    commit 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,7 +9,8 @@
 */
 int main(void)
 {
-       puts("\"Promgramming is like building a multilingual puzzle");
-       return (0);
+	puts("\"Promgramming is like building a multilingual 
+			puzzle");
+	return (0);
 }
 
