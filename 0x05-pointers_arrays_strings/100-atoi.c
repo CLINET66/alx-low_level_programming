875178e5a0fa3625c816d1049317676b168a9b55
CLINET66 <alexander66andre@gmail.com>
2022-09-19 21:56:27 -0700

    me

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-	
+
 /**
 * _atoi - convert to a int
 * @s:string
@@ -16,8 +16,10 @@ x = 1;
 while ((s[i] < '0' || s[i] > '9') && (s[i] != '\0'))
 {
 if (s[i] == '-')
+{
 x *= -1;
 i++;
+}
 }
 j = i;
 while ((s[j] >= '0') && (s[j] <= '9'))
